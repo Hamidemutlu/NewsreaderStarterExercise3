1d838c3ec68d609cbb6192ccd72e500a7d1130bd
Hamidemutlu <71645913+Hamidemutlu@users.noreply.github.com>
2021-06-16 14:41:25 +0200

    try

--- src/main/java/at/ac/fhcampuswien/newsanalyzer/ctrl/Controller.java
+++ src/main/java/at/ac/fhcampuswien/newsanalyzer/ctrl/Controller.java
@@ -1,8 +1,34 @@
 package at.ac.fhcampuswien.newsanalyzer.ctrl;
 
-public class Controller {
+import at.ac.fhcampuswien.newsapi.NewsApi;
+import at.ac.fhcampuswien.newsapi.NewsApiBuilder;
+import at.ac.fhcampuswien.newsapi.beans.Article;
+import at.ac.fhcampuswien.newsapi.beans.NewsResponse;
+import at.ac.fhcampuswien.newsapi.enums.Category;
+import at.ac.fhcampuswien.newsapi.enums.Country;
+import at.ac.fhcampuswien.newsapi.enums.Endpoint;
+import at.ac.fhcampuswien.newsapi.enums.SortBy;
 
-	public static final String APIKEY = "myAPIKey";  //TODO add your api key
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import java.util.stream.Collectors;
+
+public class Controller {
+	// My APIKEY: c944288501dc48888b5a974255d0b318;
+	public static final String APIKEY = "c944288501dc48888b5a974255d0b318";  //TODO add your api key
+	private Country country;
+	private String keyword;
+	private Category category;
+	private SortBy sortBy;
+
+	private NewsResponse newsResponse;
+
+	private Date date = Calendar.getInstance().getTime();
+	private DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+	private String strDate = dateFormat.format(date);
+
 
 	public void process() {
 		System.out.println("Start process");
@@ -12,13 +38,101 @@ public class Controller {
 		//TODO load the news based on the parameters
 
 		//TODO implement methods for analysis
-
-		System.out.println("End process");
+		NewsApi newsApi = new NewsApiBuilder()
+				.setApiKey(APIKEY)
+				.setQ(this.keyword) //				.setEndPoint(Endpoint.EVERYTHING)
+				.setEndPoint(Endpoint.TOP_HEADLINES)
+				.setFrom("2021-05-20")
+				.setSourceCountry(this.country)
+				.setSourceCategory(this.category)
+				.setSortBy(this.sortBy)
+				.createNewsApi();
+		newsResponse = newsApi.getNews();
+		/*if(newsResponse != null){
+			List<Article> articles = newsResponse.getArticles();
+			articles.stream().forEach(article -> System.out.println(article.toString()));
+		}
+		newsApi = new NewsApiBuilder()
+				.setApiKey(APIKEY)
+				.setQ("corona")
+				.setEndPoint(Endpoint.EVERYTHING)
+				.setFrom(strDate)
+				.setExcludeDomains("Lifehacker.com")
+				.createNewsApi();
+		newsResponse = newsApi.getNews();*/
 	}
-	
 
-	public Object getData() {
-		
-		return null;
+	public void setSortBy(SortBy sortBy) {
+		this.sortBy = sortBy;
+	}
+
+	public void setKeyword(String keyword){
+		this.keyword = keyword;
+	}
+
+	public void setCountry(Country country){
+		this.country = country;
+	}
+	public void setCategory(Category category){
+		this.category= category;
+	}
+	public void setDate(Date date){
+
+	}
+
+	String authorWithShortestName;
+	int numberOfArticles;
+	List<String> titles = new ArrayList<>();
+	Map<String, Integer> providers = new HashMap<>();
+
+	public void analysis(NewsResponse newsResponse){
+
+		if(newsResponse != null){
+
+			System.out.println();
+			System.out.println("Hier die Ergebnisse unserer hochentwickelten Analysesoftware: ");
+			System.out.println();
+
+			List<Article> articles = newsResponse.getArticles();
+			//List<Article> authors = articles.stream().filter(article -> article.getAuthor().length() < 10).collect(Collectors.toList());
+
+			//Number of Articles
+			numberOfArticles = (int) articles.stream().count();
+			System.out.println("Anzahl der Artikel: "+numberOfArticles);
+
+			//Which provider delivers the most articles
+			articles.stream().forEach(article -> {
+				if(!providers.containsKey(article.getSource().getName())){
+					providers.put(article.getSource().getName(), 1);
+				} else {
+					providers.put(article.getSource().getName(), providers.get(article.getSource().getName()) + 1);
+				}
+			});
+
+			String providerWithMostArticles = providers.entrySet().stream().max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getKey();
+			System.out.println("Die Quelle mit den meisten Artikeln ist: "+providerWithMostArticles);
+
+			//Author with Shortest Name
+			authorWithShortestName = articles.get(0).getAuthor();
+			articles.stream().forEach(article -> {
+				if(article != null && !(article.getAuthor() == null) && article.getAuthor().length() < authorWithShortestName.length())
+					authorWithShortestName = article.getAuthor();
+			});
+			System.out.println("Der Autor mit dem kürzesten Namen ist: "+authorWithShortestName);
+
+			//sort for length of articles
+			articles.stream().forEach(article -> {
+				if(!titles.contains(article.getTitle()))
+					titles.add(article.getTitle());
+			});
+
+			titles.sort((t1, t2) -> t2.length() - t1.length());
+			System.out.println("Hier die Titel absteigend nach Länge sortiert:");
+			titles.stream().forEach(System.out::println);
+		}
+	}
+
+	public NewsResponse getData() {
+		return newsResponse;
 	}
 }
--- src/main/java/at/ac/fhcampuswien/newsanalyzer/ui/UserInterface.java
+++ src/main/java/at/ac/fhcampuswien/newsanalyzer/ui/UserInterface.java
@@ -2,23 +2,53 @@ package at.ac.fhcampuswien.newsanalyzer.ui;
 
 
 import at.ac.fhcampuswien.newsanalyzer.ctrl.Controller;
+import at.ac.fhcampuswien.newsapi.beans.Article;
+import at.ac.fhcampuswien.newsapi.beans.NewsResponse;
+import at.ac.fhcampuswien.newsapi.enums.Category;
+import at.ac.fhcampuswien.newsapi.enums.Country;
+import at.ac.fhcampuswien.newsapi.enums.SortBy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Scanner;
 
 public class UserInterface 
 {
+	Scanner scanner = new Scanner(System.in);
 	private Controller ctrl = new Controller();
 
 	public void getDataFromCtrl1(){
 		System.out.println("ABC");
-
+		ctrl.setKeyword("corona");
+		ctrl.setCountry(Country.at);
+		ctrl.setCategory(Category.health);
+		ctrl.setSortBy(SortBy.RELEVANCY);
 		ctrl.process();
+		NewsResponse newsResponse = ctrl.getData();
+
+		if(newsResponse != null){
+			List<Article> articles = newsResponse.getArticles();
+			articles.stream().forEach(article -> System.out.println(article.toString()));
+		}
 	}
 
 	public void getDataFromCtrl2(){
 		// TODO implement me
+		ctrl.setKeyword("Sport");
+		ctrl.setCountry(Country.at);
+		ctrl.setCategory(Category.sports);
+		ctrl.process();
+
+		NewsResponse newsResponse = ctrl.getData();
+
+		if(newsResponse != null){
+			List<Article> articles = newsResponse.getArticles();
+			articles.stream().forEach(article -> System.out.println(article.toString()));
+		}
+
+		ctrl.analysis(newsResponse);
 	}
 
 	public void getDataFromCtrl3(){
@@ -27,22 +57,82 @@ public class UserInterface
 	
 	public void getDataForCustomInput() {
 		// TODO implement me
+		boolean categorySet = false;
+		boolean analysisSet = false;
+
+		while(!categorySet){
+			System.out.println("Wählen Sie eine der folgenden Kategorien: ");
+			System.out.print(
+					"b: Business\n"+
+							"u: Unterhaltung\n"+
+							"g: Gesundheit\n"+
+							"w: Wissenschaft\n"+
+							"t: Technologie\n"+
+							"> ");
+
+			String s = scanner.next();
+			switch(s){
+				case "b": ctrl.setCategory(Category.business); categorySet=true; break;
+				case "u": ctrl.setCategory(Category.entertainment); categorySet=true; break;
+				case "g": ctrl.setCategory(Category.health); categorySet=true; break;
+				case "w": ctrl.setCategory(Category.science); categorySet=true; break;
+				case "t": ctrl.setCategory(Category.technology); categorySet=true; break;
+				default:
+					System.out.println("Ungültige Eingabe"); break;
+			}
+			System.out.println();
+		}
+
+		System.out.print("Geben Sie ein Schlüsselwort ein: ");
+		String s2 = scanner.next();
+		ctrl.setKeyword(s2);
+		System.out.println();
+
+		ctrl.setCountry(Country.at);
+		ctrl.setKeyword("");
+
+
+		System.out.println("Hier Ihre Nachrichten in Österreich:");
+
+		ctrl.process();
+
+		NewsResponse newsResponse= ctrl.getData();
+
+		while(!analysisSet){
+			System.out.print("Möchten Sie unseren Algorithmus verwenden um Ihre Nachrichten zu analysieren? (j/n): ");
+			String s3 = scanner.next();
+			switch(s3){
+				case "j": ctrl.analysis(newsResponse); analysisSet=true; break;
+				case "n": analysisSet=true; break;
+				default: System.out.println("Ungültige Eingabe"); break;
+			}
+			System.out.println();
+		}
+
+		if(newsResponse != null){
+			List<Article> articles = newsResponse.getArticles();
+			articles.stream().forEach(article -> System.out.println(article.toString()));
+		}
 	}
 
 
 	public void start() {
-		Menu<Runnable> menu = new Menu<>("User Interface");
-		menu.setTitle("Wählen Sie aus:");
-		menu.insert("a", "Choice ABC", this::getDataFromCtrl1);
-		menu.insert("b", "Choice DEF", this::getDataFromCtrl2);
-		menu.insert("c", "Choice 3", this::getDataFromCtrl3);
-		menu.insert("d", "Choice User Input:",this::getDataForCustomInput);
-		menu.insert("q", "Quit", null);
-		Runnable choice;
-		while ((choice = menu.exec()) != null) {
-			 choice.run();
+		try {
+			Menu<Runnable> menu = new Menu<>("User Interface");
+			menu.setTitle("Wählen Sie aus:");
+			menu.insert("a", "Top Nachrichten zu COVID-19 in Österreich", this::getDataFromCtrl1);
+			menu.insert("b", "Alle News zu Sport in Österreich", this::getDataFromCtrl2);
+			menu.insert("c", "Choice 3", this::getDataFromCtrl3);
+			menu.insert("d", "Choice User Input:", this::getDataForCustomInput);
+			menu.insert("q", "Quit", null);
+			Runnable choice;
+			while ((choice = menu.exec()) != null) {
+				choice.run();
+			}
+			System.out.println("Program finished");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
-		System.out.println("Program finished");
 	}
 
 
